ea73cd64bc47264cdc1f9e59384a7e149746e5d4
Jeongsohee <toy3045@gmail.com>
2024-07-20 02:52:22 +0900

    📦 Chore: prettier, eslint 환경 재설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
+  root: true,
   env: {
-    node: true,
-    commonjs: true,
-    es2021: true,
+    browser: true,
+    es2020: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react-refresh'],
   rules: {
-    "prettier/prettier": "error",
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
 };
