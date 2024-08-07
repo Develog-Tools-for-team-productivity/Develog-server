2a192486d66125d672acf2df9fc94e27c99cd216
Jeongsohee <toy3045@gmail.com>
2024-08-07 22:12:27 +0900

    📦 Chore: cors origin 주소 수정

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://develog-tools.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
